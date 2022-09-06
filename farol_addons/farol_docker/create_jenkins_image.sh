ec2e16985c1c58074da405e6523619b1775a3ef9
jquintas <jquintas@gmail.com>
2022-09-06 12:15:59 +0100

    Documentation (#2)
    * Change(cicd): Change folder test name to farol_cicd_test
    
    * Del(cicd): Remove uncessary files
    
    * Update(cicd): Change docker folder to addons. Add it to mkdocs.yml

--- farol_addons/farol_docker/create_jenkins_image.sh
+++ farol_addons/farol_docker/create_jenkins_image.sh
@@ -1,4 +1,4 @@
-LATEST_VERSION=v0.0.3
+LATEST_VERSION=v0.0.4
 
 docker build --no-cache -t farol_jenkins:${LATEST_VERSION} .
 docker tag farol_jenkins:${LATEST_VERSION} farol_jenkins:latest
@@ -11,3 +11,7 @@ docker tag farol_jenkins:${LATEST_VERSION} harbor.dsor.isr.tecnico.ulisboa.pt/fa
 docker push harbor.dsor.isr.tecnico.ulisboa.pt/farol/farol_jenkins:${LATEST_VERSION}
 docker tag farol_jenkins:latest harbor.dsor.isr.tecnico.ulisboa.pt/farol/farol_jenkins:latest
 docker push harbor.dsor.isr.tecnico.ulisboa.pt/farol/farol_jenkins:latest
+
+# Add the version and latest tags to dockerhub
+docker tag harbor.dsor.isr.tecnico.ulisboa.pt/farol/farol_jenkins:latest dsorisr/farol_jenkins:latest
+docker push dsorisr/farol_jenkins:latest
