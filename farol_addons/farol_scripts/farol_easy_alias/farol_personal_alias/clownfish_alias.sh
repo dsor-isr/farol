c5a3a93a3579ddc0471b23a9eae353aae0063eea
Quintas <jquintas@gmail.com>
2022-10-06 15:48:35 +0100

    Update(farol_addons): Delete unused scripts and update personal alias

--- farol_addons/farol_scripts/farol_easy_alias/farol_personal_alias/clownfish_alias.sh
+++ farol_addons/farol_scripts/farol_easy_alias/farol_personal_alias/clownfish_alias.sh
@@ -2,24 +2,18 @@
 
 # FAROL SSH connections
 
-alias myellow='ssh farol@myellow'          # Myellow
-alias mredLab='ssh farol@mredLab'          # Mred lab
-alias mblackLab='ssh isr@mblackLab'         # Mblack lab
-alias delfim='ssh dsor@delfim'              # Delfim
-alias muned='ssh farol@muned'              # Muned
-alias mblack='ssh farol@mblack'            # Mblack
-alias mred='ssh farol@mred'                # Mred
+alias myellow='ssh medusa@myellow'          # Myellow
+alias delfim='ssh delfim@delfim'              # Delfim
+alias muned='ssh medusa@muned'              # Muned
+alias mblack='ssh medusa@mblack'            # Mblack
+alias mred='ssh medusa@mred'                # Mred
 alias hrov_pts_otg='ssh oceantech@hrov_otg' # Hrov port side
-alias mvector='ssh farol@mvector'	        # Mvector
+alias mvector='ssh medusa@mvector'	        # Mvector
 
 # *.* FAROL Safety Features
-alias sf_myellow='$(find ${ROS_WORKSPACE}/src/ -type d -iname safety_feature)/src/Client/SafetyFeatureCLT myellow'
-alias sf_mvector='$(find ${ROS_WORKSPACE}/src/ -type d -iname safety_feature)/src/Client/SafetyFeatureCLT mvector'
-alias sf_mred='$(find ${ROS_WORKSPACE}/src/ -type d -iname safety_feature)/src/Client/SafetyFeatureCLT mred'
-alias sf_mblack='$(find ${ROS_WORKSPACE}/src/ -type d -iname safety_feature)/src/Client/SafetyFeatureCLT mblack'
-alias sf_delfim='$(find ${ROS_WORKSPACE}/src/ -type d -iname safety_feature)/src/Client/SafetyFeatureCLT delfim'
-alias sf_muned='$(find ${ROS_WORKSPACE}/src/ -type d -iname safety_feature)/src/Client/SafetyFeatureCLT muned'
-
-# VPN DSOR
-alias vpn_dsor_on='sudo systemctl start wg-quick@vpn-dsor'
-alias vpn_dsor_off='sudo systemctl stop wg-quick@vpn-dsor'
+alias sf_myellow='$(find ${ROS_WORKSPACE}/src/ -type d -iname watchdog)/src/WatchdogClient/watchdog_client myellow'
+alias sf_mvector='$(find ${ROS_WORKSPACE}/src/ -type d -iname watchdog)/src/WatchdogClient/watchdog_client mvector'
+alias sf_mred='$(find ${ROS_WORKSPACE}/src/ -type d -iname watchdog)/src/WatchdogClient/watchdog_client  mred'
+alias sf_mblack='$(find ${ROS_WORKSPACE}/src/ -type d -iname watchdog)/src/WatchdogClient/watchdog_client mblack'
+alias sf_delfim='$(find ${ROS_WORKSPACE}/src/ -type d -iname watchdog)/src/WatchdogClient/watchdog_client  delfim'
+alias sf_muned='$(find ${ROS_WORKSPACE}/src/ -type d -iname watchdog)/src/WatchdogClient/watchdog_client muned'
